90aa92db2bebac280dc5380aeda83e03ff71281e
adi5765879 <77149386+adi5765879@users.noreply.github.com>
2021-03-26 20:59:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("hi i am aditya ");
+var first_name="aditya";
+var last_name="ray";
+console.log (first_name);
+console.log(last_name);
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var length=full_name.length;
+var replace=full_name.replace("ray","kumar");
+console.log(replace);
+console.log(length);
+var num1=16;
+var num2=15;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1/num2);
+console.log(num1*num2);
+
